dbdbb948834ebc961ba83d7eec7aafeff7803cc8
dugduy <102119666+dugduy@users.noreply.github.com>
2022-03-23 18:49:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
 from flask import Flask
-from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
+    return "My Hello World!!!"
 app.run(debug=True, use_reloader=True)
